3a2f21c10eecfbb6fb81bea1a43dcfe553245a02
sefabaygul <110894002+sefabaygul@users.noreply.github.com>
2022-08-18 16:01:10 +0300

    T3

--- src/C01_team7Deneme.java
+++ src/C01_team7Deneme.java
@@ -2,5 +2,6 @@ public class C01_team7Deneme {
     public static void main(String[] args) {
         System.out.println("team7 deneme 1");
         System.out.println("team7 deneme 2");
+        System.out.println("team7 deneme 3");
     }
 }
